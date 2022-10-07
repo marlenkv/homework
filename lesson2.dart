cbd4ebf7a6fd148fadf2836e359473a4fccd8943
marlenkv <kumarbekovmarlen@gmail.com>
2022-10-07 19:51:49 +0600

    first commit

---
+++ lesson2.dart
@@ -0,0 +1,25 @@
+void main() {
+  String name = "marlen";
+  int mark = 40;
+  double pi = 4;
+  int a = 10;
+  int b = 20;
+  String msg = "добро пожаловать во Flutter";
+  String msg1 = 'добро пожаловать во Flutter';
+  List array = [
+    12,
+    "sda",
+    true,
+  ];
+  bool isNumber = true;
+  bool isString = false;
+  print(array);
+  print(isNumber);
+  print(isString);
+  print(msg);
+  print(msg1);
+  print(a + b);
+  print(name);
+  print(mark);
+  print(pi);
+}
